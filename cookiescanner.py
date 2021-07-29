2533e2b4ba9fce05c3e860ee5f987cfcac834474
Christiano DAvila Medeiros <comedeiros@acknology.com.br>
2021-07-29 17:35:43 -0300

    Release inicial

---
+++ cookiescanner.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+
+# Instalação:
+# sudo yum install chromium-chromedriver
+# sudo yum install firefox
+# sudo -H python3 -m pip install selenium
+# sudo -H python3 -m pip install flask
+
+from flask import Flask, request, jsonify
+import sys
+import json
+import time
+import pickle
+import sqlite3
+import os
+#import xlsxwriter as xw
+import glob
+from datetime import datetime as dt
+from selenium import webdriver 
+
+# Chrome ==========================================================
+#from selenium.webdriver.chrome.options import Options
+#chrome_options = Options()
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--no-sandbox") # linux only
+#chrome_options.headless = True # also works
+#driver = webdriver.Chrome(options=chrome_options)
+# =================================================================
+
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.headless = True
+
+app = Flask(__name__)
+# app.config["DEBUG"] = True
+
+hdr = ['Cookie', 'Dominio', 'Expiracao', 'HttpOnly', 'Path', 'SameSite', 'Secure', 'Value', 'EU_IA', 'Plataforma', 'Categoria', 'DscDominio', 'Retencao', 'Controlador', 'Politica' , 'Descricao']
+
+coringas = []
+
+try:
+	con = sqlite3.connect('./db/cookies_db.db')
+	cur = con.cursor()
+
+	query = """ 
+				select
+						nome
+				from
+						all_cookies
+				where
+						coringa = 1
+				order by
+						nome
+			"""
+	ds = cur.execute(query)
+
+	for item in ds:
+
+		coringas.append(item[0])
+
+	coringas.sort(key=len, reverse=True)
+
+except:
+	print(">>> %s - Erro ao conectar a base de cookies para efetuar a carga dos cookies variantes..."%(dt.now()))
+
+finally:
+	con.close()
+
+def getCookieName(cookieStr):
+
+	for cookie in coringas:
+		if cookie in cookieStr:
+			 return(cookie)
+	return cookieStr
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    return '''<h1>Scanner de Cookies</h1>
+<p>Módulo Security - CookieScanner - API Scanner de Cookies.</br>
+Autor: Christiano Medeiros - christiano.medeiros (at) modulo.com</p>'''
+
+@app.route('/api/v1/cookies/scan', methods=['GET'])
+
+def api_id():
+
+	try:
+		con = sqlite3.connect('./db/cookies_db.db')
+		cur = con.cursor()
+	except e:
+		print(">>> %s - Erro ao conectar a base de cookies..."%(dt.now(),e))
+
+	xrow=0
+	xcol=0
+
+	if 'uri' in request.args:
+		uri = request.args['uri']
+
+		cookies_json = "cookies_%s.json"%uri.replace('/','-')
+		json_path = "./json/"
+
+		print(">>> %s - Iniciando o WebDriver..."%dt.now())
+		driver = webdriver.Firefox(options=options)
+
+		try:
+			print(">>> %s - Acessando %s..."%(dt.now(),uri))
+			driver.get("%s"%uri)
+		except Exception as e:
+			print("### Erro: %s"%str(e))
+
+
+		print(">>> %s - Gravando %s..."%(dt.now(),cookies_json))
+		cookies = driver.get_cookies()
+		with open(json_path+cookies_json, 'w+', newline='') as outputdata:
+		    json.dump(cookies, outputdata,indent=4, sort_keys=True)
+
+		driver.quit()
+
+	else:
+		return "Erro: Nenhuma uri informada. Por favor, especifique uma uri para efetuar o scan."
+
+	result={}
+
+	for ck in cookies:
+		analise = {}
+		xds = []
+		xcol = 0
+
+		query = """ 
+					select 	EU_IA,
+						 	plataforma,
+						 	categoria,
+						 	dominio,
+						 	retencao,
+						 	controlador,
+						 	politica,
+						 	descricao 
+					from
+							all_cookies
+					where
+							nome like \'{}%\'
+					order by
+							nome
+					limit 1
+
+				""".format(getCookieName(ck['name']))
+
+		#executando a quey na base all_cookies (base de cookies)
+		ds = cur.execute(query)
+
+		#criando lista contendo dados dos cookies coletados na web
+		try:
+			xds = [ck['name'],ck['domain'], dt.fromtimestamp(int(ck['expiry'])).strftime('%Y-%m-%d %H:%M:%S'), ck['httpOnly'], ck['path'], ck['sameSite'], ck['secure'], ck['value']]
+		except:
+			xds = [ck['name'],ck['domain'], '-1', ck['httpOnly'], ck['path'], ck['sameSite'], ck['secure'], ck['value']]
+
+		#Incluindo na lista 'xds' campos de retorno via SQL da base de cookies
+		for r in ds:
+			for v in r:
+				xds.append(v)
+
+		#Populando dicionário 'analise' com os dados contidos na lista 'xds'
+		for c in hdr:
+			try:
+				analise[c]=xds[hdr.index(c)]
+			except:
+				analise[c]=''
+
+		# print("CK:",ck)
+
+		# print("ANALISE:",analise)
+
+		# print("RESULT:",result)
+
+		result[getCookieName(ck['name'])]=analise
+
+	con.close()
+
+	print(">>> %s - Aguardando conexão..."%dt.now())
+
+	endRes=dict()
+
+	endRes[uri]=result
+
+	return jsonify(endRes)
+
+if __name__ == '__main__':
+	print(">>> %s - Iniciando o servidor..."%dt.now())
+	app.run(port=5000,debug=True) 
---
+++ json2xlsx.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import sqlite3
+import json
+import os
+import sys
+import datetime
+import xlsxwriter as xw
+import glob
+
+from datetime import datetime as dt
+
+filel=glob.glob("./json/*.json")
+
+xfile = sys.argv[1]
+
+wb = xw.Workbook("./xlsx/{}.xlsx".format(xfile))
+
+for jfile in filel:
+
+	print(">> %s - Lendo o arquivo %s"%(dt.now(),jfile))
+
+	f = open("%s"%jfile,)
+	js = json.load(f)
+
+	print(">> %s - Criando a planilha %s"%(dt.now(),jfile.split("_")[1][:-5]))
+	ws = wb.add_worksheet(jfile.split("_")[1][:-5])
+
+	con = sqlite3.connect('./db/cookies_db.db')
+	cur = con.cursor()
+
+	hdr = (['Cookie', 'Dominio', 'Expiracao', 'HttpOnly', 'Path', 'SameSite', 'Secure', 'Value', 'EU_AI', 'Plataforma', 'Categoria', 'DscDominio', 'Retencao', 'Controlador', 'Politica' , 'Descricao'])
+
+	xrow=0
+	xcol=0
+
+	for c in hdr:
+	   ws.write(xrow,xcol,c)
+	   xcol=xcol+1
+
+	xrow=1
+
+	for ck in js:
+		xcol = 0
+		query = "select EU_IA, plataforma, categoria, dominio, retencao, controlador, politica, descricao from all_cookies where nome like \'{}%\' order by nome limit 1".format(ck['name'])
+		#print(query)
+		#pass
+		ds = cur.execute(query)
+		try:
+			#xds = ([ck['domain'], ck['expiry'], ck['httpOnly'], ck['path'], ck['sameSite'], ck['secure'], ck['value']])
+			xds = ([ck['name'],ck['domain'], datetime.datetime.fromtimestamp(int(ck['expiry'])).strftime('%Y-%m-%d %H:%M:%S'), ck['httpOnly'], ck['path'], ck['sameSite'], ck['secure'], ck['value']])
+		except:
+			xds = ([ck['name'],ck['domain'], '-1', ck['httpOnly'], ck['path'], ck['sameSite'], ck['secure'], ck['value']])
+
+		for r in ds:
+			xds.append(r[0])
+			xds.append(r[1])
+			xds.append(r[2])
+			xds.append(r[3])
+			xds.append(r[4])
+			xds.append(r[5])
+			xds.append(r[6])
+			xds.append(r[7])
+		for c in xds:
+			#print("xrow:%d, xcol:%d, xds:%s"%(xrow,xcol,xds))
+			ws.write(xrow,xcol,xds[xcol])
+			xcol = xcol + 1
+		xrow = xrow + 1
+
+print(">> %s - Gravando o arquivo ./xlsx/%s.xlsx"%(dt.now(),xfile))
+wb.close()
+con.close()
+
+
+#cell_format.set_font_color('#FF0000')
+#cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
+
+
+
+
+
+
+
+
